013e27dc79c1de417269bc2261477c7e80c34e7e
box755 <kk20031022@gmail.com>
2025-04-29 02:11:04 +0800

    installed scss auto import packages, installed element-plus package, started of login page.

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,10 +3,24 @@ import { createRouter, createWebHistory } from 'vue-router'
 // 匯入頁面元件
 import Home from '../views/HomePage.vue'
 import About from '../views/AboutPage.vue'
+import Login from '../views/login/index.vue'
 
 const routes = [
-  { path: '/', name: 'Home', component: Home },
-  { path: '/about', name: 'About', component: About },
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  }
 ]
 
 const router = createRouter({
